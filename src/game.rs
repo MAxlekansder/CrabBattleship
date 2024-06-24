fdaf439ad3711c5194a109fb38b9a82979c964b0
MAxlekansder <alexander.hasslund@stud.sti.se>
2024-06-24 16:01:29 +0200

    refactored the game logic into functions

--- src/game.rs
+++ src/game.rs
@@ -6,25 +6,25 @@ pub fn start_game() {
     let mut player_board = Board::new();
     let mut opponent_board = Board::new();
 
-    player_board.place_ship(5);
-    player_board.place_ship(4);
-    player_board.place_ship(3);
-    player_board.place_ship(2);
-
-    opponent_board.place_ship(5);
-    opponent_board.place_ship(4);
-    opponent_board.place_ship(3);
-    opponent_board.place_ship(2);
-
+    place_ships(&mut player_board);
+    place_ships(&mut opponent_board);
 
     loop {
-        print!("\x1b[2J\x1b[1;1H");
+        clear_screen();
 
-        println!("\x1b[1;37mYour board:\x1b[0m");
-        player_board.display(false);
-        println!("\x1b[1;37mOpponents board:\x1b[0m");
-        opponent_board.display(true);
+        display_board(&player_board, &opponent_board);
 
+        if handle_opponent_turn(&mut opponent_board) {
+            break;
+        }
+
+        if handle_player_turn(&mut player_board) {
+            break;
+        }
+    }
+
+
+    fn handle_player_turn(opponent_board: &mut Board) -> bool {
         let (player_row, player_col) = get_player_input();
         let result = opponent_board.fire(player_row, player_col);
 
@@ -39,13 +39,13 @@ pub fn start_game() {
 
         if opponent_board.is_game_over() {
             println!("\x1b[1;32mYou won the game!\x1b[0m");
-            break;
+            return true;
         }
+        false
+    }
 
-        if player_board.is_game_over() {
-            println!("\x1b[1;31mYou lost the game!\x1b[0m");
-            break;
-        }
+
+    fn handle_opponent_turn(player_board: &mut Board) -> bool {
 
         let (opponent_row, opponent_col) = generate_opponent_move();
         let result = player_board.fire(opponent_row, opponent_col);
@@ -58,6 +58,31 @@ pub fn start_game() {
         println!("Press enter to continue...");
         io::stdin().read_line(&mut String::new()).expect("Failed to read input");
 
+        if player_board.is_game_over() {
+            println!("\x1b[1;31mYou lost the game!\x1b[0m");
+            return true;
+        }
+        false
     }
 
+
+    fn display_board(player_board: &Board, opponent_board: &Board) {
+        println!("\x1b[1;37mYour board:\x1b[0m");
+        player_board.display(false);
+        println!("\x1b[1;37mOpponents board:\x1b[0m");
+        opponent_board.display(true);
+    }
+
+
+    fn place_ships(board: &mut Board) {
+        board.place_ship(5);
+        board.place_ship(4);
+        board.place_ship(3);
+        board.place_ship(2);
+    }
+
+
+    fn clear_screen() {
+        print!("\x1b[2J\x1b[1;1H");
+    }
 }
